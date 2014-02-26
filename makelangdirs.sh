5e53443f967984aa2d1d10783d2cef03800c0dec
Kevin McCoy <kevin@qloog.com>
2014-02-26 18:10:47 +0900

    Updated README with script explanation

--- makelangdirs.sh
+++ makelangdirs.sh
@@ -40,5 +40,5 @@ cd /var/www/html/admin/
 for testdir in `find . -type d -name "i18n"`
 do
 	[ -d $testdir/ja_JP/LC_MESSAGES ] || echo $testdir "was missing a" $LANGCODE "directory";
-        [ -d $testdir/ja_JP/LC_MESSAGES ] || mkdir -p $testdir/ja_JP/LC_MESSAGES;	
+        [ -d $testdir/ja_JP/LC_MESSAGES ] || mkdir -p $testdir/$LANGCODE/LC_MESSAGES;	
 done
